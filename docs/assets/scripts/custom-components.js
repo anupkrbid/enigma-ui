1d91aa4f53b46a75ae37548c1d4f358b37dd64c9
Anup Kumar Bid <anupbid94@gmail.com>
2022-02-09 13:00:08 +0530

    feat: implemented responsivenes

--- docs/assets/scripts/custom-components.js
+++ docs/assets/scripts/custom-components.js
@@ -14,25 +14,58 @@ class Header extends HTMLElement {
               )}" onclick="finesse.toggleSidebar('#sidebar')">
                 <i class="fas fa-bars"></i>
               </button>
-              <div class="nav-brand">fιиєѕѕє υι</div>
-              <ui class="list list-inline">
-              <li><a class="nav-link ${this.isLinkActive(
-                activeNav,
-                "home"
-              )}" href="/index.html">Home</a></li>
-              <li>
-                <a class="nav-link ${this.isLinkActive(
+              <!-- <div class="nav-brand">fιиєѕѕє υι</div>-->
+              <div class="nav-brand">Finesse UI</div>
+              <ui class="list list-inline nav-content-hidden d-flex-tablet-onwards">
+                <li><a class="nav-link ${this.isLinkActive(
                   activeNav,
-                  "documentation"
-                )}" href="/docs/introduction.html">Documentation</a>
-              </li>
+                  "home"
+                )}" href="/index.html">Home</a></li>
+                <li>
+                  <a class="nav-link ${this.isLinkActive(
+                    activeNav,
+                    "documentation"
+                  )}" href="/docs/introduction.html">Documentation</a>
+                </li>
               </ui>
             </div>
-            <div class="nav-content nav-content-right">
+            <div class="nav-content nav-content-right nav-content-hidden d-block-tablet-onwards">
               <button class="btn btn-icon">
-                <i class="fas fa-moon"></i>
+                <i class="fas fa-adjust"></i>
               </button>
-              <button class="btn btn-primary">Download</button>
+              <button class="btn btn-primary">
+                 Download
+              </button>
+            </div>
+            <div class="nav-content nav-content-mobile d-none-tablet-onwards">
+              <button class="btn btn-icon">
+                <i class="fas fa-adjust"></i>
+              </button>
+              <div class="dropdown" data-dropdown>
+                <button class="btn" data-dropdown-trigger>
+                  Menu
+                  <i class="fas fa-caret-square-down"></i>
+                </button>
+                <div class="dropdown-menu">
+                  <ul class="list align-items-stretch">
+                    <li><a class="nav-link ${this.isLinkActive(
+                      activeNav,
+                      "home"
+                    )}" href="/index.html">Home</a></li>
+                    <li>
+                      <a class="nav-link ${this.isLinkActive(
+                        activeNav,
+                        "documentation"
+                      )}" href="/docs/introduction.html"
+                        >Documentation</a
+                      >
+                    </li>
+                    <li>
+                      <button class="btn btn-primary is-100">Download</button>
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </div>
           </nav>
         </header>
@@ -118,17 +151,17 @@ class Footer extends HTMLElement {
       <ul class="list list-inline">
         <li>
           <a target="_blank" href="https://github.com/anupkrbid"
-            ><i class="fa-brands fa-github-square"></i
+            ><i class="fab fa-github-square"></i></i
           ></a>
         </li>
         <li>
           <a target="_blank" href="https://twitter.com/anupkrbid"
-            ><i class="fa-brands fa-twitter-square"></i
+            ><i class="fab fa-twitter-square"></i></i
           ></a>
         </li>
         <li>
           <a target="_blank" href="https://www.linkedin.com/in/anupkrbid/"
-            ><i class="fa-brands fa-linkedin"></i
+            ><i class="fab fa-linkedin"></i></i
           ></a>
         </li>
       </ul>
--- src/js/finesse.js
+++ src/js/finesse.js
@@ -9,3 +9,30 @@ const finesse = {
     }
   },
 };
+
+// event lister to handle dropdown click
+document.addEventListener("click", ($event) => {
+  // chh if dropdown was clicked
+  const isDropdownTriggered = $event.target.matches("[data-dropdown-trigger]");
+  const closestTriggeredDropdown = $event.target.closest("[data-dropdown]");
+
+  // if user clicked inside  a dropdown or didn't click on a dropdown then ignore
+  if (!isDropdownTriggered && closestTriggeredDropdown !== null) {
+    return;
+  }
+
+  // if drop down was triggered add active class
+  if (isDropdownTriggered) {
+    closestTriggeredDropdown.classList.toggle("dropdown-active");
+  }
+
+  // remove activ class from already opened dropdowns
+  document
+    .querySelectorAll("[data-dropdown].dropdown-active")
+    .forEach((dropdown) => {
+      if (dropdown === closestTriggeredDropdown) {
+        return;
+      }
+      dropdown.classList.remove("dropdown-active");
+    });
+});
